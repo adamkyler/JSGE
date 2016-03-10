48a36b826fc5a624c28dd8eec3ac5e937ff33c65
kyler <kyler@kyler.kyler>
2016-03-09 20:15:12 -0500

    Added timers and alerts

--- JSGE.js
+++ JSGE.js
@@ -9,6 +9,7 @@
  *  Detecting and binding keypresses, mouse movements, etc
  */
 
+
 function JSGE(canvas){
 	
 	//Get a handle on the canvas, create context
@@ -22,9 +23,51 @@ function JSGE(canvas){
 	//Initialize list of pressed keys
 	this.keys = [];
 	
+	//Initialize alerts
+	this.alerts = []
+	
+	
+	this.lastTime = null;
+	this.timer = null;
+	this.tickCb = null;
+	
+}
+
+//Sets a recurring timer
+JSGE.prototype.setTimer = function(cb,dur){
+	this.tickCb = cb;
+	this.lastTime = new Date().getTime();
+	this.timer = setInterval(this.tick.bind(this),dur);
+}
+//Called after timer is set, passes elapsed time to callback function
+JSGE.prototype.tick = function(){
+	var cTime = new Date().getTime();
+	this.tickCb(cTime-this.lastTime);
+	this.lastTime = cTime;
 }
 
 
+//Global alert, recieves JSON and stores in buffer
+JSGE.prototype.alert = function(alt){
+	this.alerts.push(alt);
+}
+//Gets the next alert from the queue
+JSGE.prototype.getAlert = function(){
+	return this.alerts.shift();
+}
+//Returns true if there is an alert in the queue
+JSGE.prototype.hasAlert = function(){
+	return this.alerts.length>0;
+}
+
+
+//Returns the context for drawing 
+JSGE.prototype.getCtx = function(){
+	return this.ctx;
+}
+
+
+//Functions bound to event listeners
 JSGE.prototype.keyDown = function(e){
 	this.keys[e.keyCode] = true;
 }
